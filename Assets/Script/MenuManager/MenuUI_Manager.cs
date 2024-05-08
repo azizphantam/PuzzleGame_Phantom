2d805ffbcc7e928a7133d5bd7f11563a43f3fad8
azizphantam <168703306+azizphantam@users.noreply.github.com>
2024-05-08 11:51:11 +0500

    settings_ui

---
+++ Assets/Script/MenuManager/MenuUI_Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI_Manager : MonoBehaviour
+{
+    public GameObject Setting;
+    public GameObject MenuPanel;
+    public GameObject RateUs;
+    
+}
